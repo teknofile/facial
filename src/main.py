63ad25e77af6d73149cb87cc59f26420f2ce6cbb
James Richardson <teknofile@teknofile.org>
2019-09-25 22:24:56 -0600

    script can now detect a # of faces in an image

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from app import app
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
+from PIL import Image
+import face_recognition
+
 ALLOWED_EXTENSION = set( [ 'png', 'jpg', 'jpeg' ] )
 
 def allowed_file(filename):
@@ -25,10 +28,19 @@ def upload_file():
                         flash('No file selected for uploading')
                         return redirect(request.url)
                 if file and allowed_file(file.filename):
-                        filename = secure_filename(file.filename)
-                        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                        flash('File successfully uploaded')
-                        return redirect('/')
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+                    #file = request.files.get('file')
+                    #    img = file.stream.read()
+                    image = face_recognition.load_image_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    face_locations = face_recognition.face_locations(image)
+                    flash("Found {} face(s) in the photo.".format(len(face_locations)))
+
+                    #filename = secure_filename(file.filename)
+                    #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    flash('File successfully uploaded')
+                    return redirect('/')
                 else:
                         flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
                         return redirect(request.url)
